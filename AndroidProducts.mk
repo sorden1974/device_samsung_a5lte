8a258bda24a24aed2094631e4ae288477c2caa53
srkndenis <srkndenis@gmail.com>
2021-04-26 16:41:33 +0000

    a5lte: Bring to Havoc-OS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a5lte.mk
+    $(LOCAL_DIR)/havoc_a5lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a5lte-user \
-    lineage_a5lte-userdebug \
-    lineage_a5lte-eng
+    havoc_a5lte-user \
+    havoc_a5lte-userdebug \
+    havoc_a5lte-eng
